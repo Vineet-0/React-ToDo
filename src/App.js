6055e63fea78552a6033b536e144405705ce9ae3
Vineet-0 <vineet351016@gmail.com>
2023-07-18 23:40:26 +0530

    Hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { ToDo } from './components/ToDo';
+import { ToDoWrapper } from './components/ToDoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoWrapper />
+    </div>
+  );
+}
+
+export default App;
